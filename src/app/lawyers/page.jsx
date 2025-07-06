7f28d2dd9a2c7e1974e555b86a360b2c72368567
Pratikaryal23 <aryalpartik057@gmail.com>
2025-07-06 23:27:01 +0545

    first commit

---
+++ src/app/lawyers/page.jsx
@@ -0,0 +1,13 @@
+import Card from '@/Components/Card'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-white h-screen'>
+
+    <Card/>
+    </div>
+  )
+}
+
+export default page
