1b106030f27e8210fd74df802ac9aaed915a62ae
Pratikaryal23 <aryalpartik057@gmail.com>
2025-07-06 23:31:09 +0545

    changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,8 @@
 'use client'
 
 import React, { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+// import { Button } from "@/components/ui/button"
+// import { Input } from "@/components/ui/input"
 import {
   Select,
   SelectContent,
